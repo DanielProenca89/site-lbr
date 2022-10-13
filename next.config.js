d11b778f4fb43534858caac4267937e1ef432ab7
DanielProenca89 <danielproenca89@gmail.com>
2022-10-13 03:51:19 -0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  test: /\.svg$/,
+  use: ["@svgr/webpack"]
+  
+}
+
+module.exports = nextConfig
+
